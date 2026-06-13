bb0a59efbfe5224a7586c104f5d4725b2c9615e4
SEGUER <106338528+seguertv@users.noreply.github.com>
2026-06-13 16:35:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const asesores = [
- {nombre:"FLAVIA",ventas:9,color:"#FF0000"},
- {nombre:"MARILY",ventas:7,color:"#0066FF"},
- {nombre:"LEONARDO",ventas:6,color:"#FFD700"},
- {nombre:"ESTEFANI",ventas:4,color:"#00CC00"},
- {nombre:"ISRAEL",ventas:3,color:"#FF7F00"},
- {nombre:"PEDRO",ventas:2,color:"#9400D3"},
- {nombre:"LUIS",ventas:1,color:"#00CED1"}
-];
+let asesores = [];
+
+async function cargarDatos(){
+
+ const url =
+ "https://docs.google.com/spreadsheets/d/e/2PACX-1vReYYJAd7o9nKtKL-ynq9ckTFdobNYJHZqgrfqHDFvzcMlPOkR-vjzDB7rYoHO4hNaaw1wOxnDyo9_T/pub?gid=731585806&single=true&output=tsv";
+
+ const respuesta = await fetch(url);
+
+ const texto = await respuesta.text();
+
+ const filas = texto.trim().split("\n");
+
+ asesores = [];
+
+ for(let i=1;i<filas.length;i++){
+
+   const columnas = filas[i].split("\t");
+
+   if(columnas.length < 3) continue;
+
+   asesores.push({
+      nombre: columnas[0],
+      ventas: Number(columnas[1]),
+      color: columnas[2]
+   });
+
+ }
+
+cargarDatos();
+
+}
 
 const pista = document.getElementById("pista");
 const ranking = document.getElementById("ranking");
@@ -68,4 +91,8 @@ pista.appendChild(nombre);
  document.getElementById("hora").innerText=
  ahora.toLocaleTimeString();
 
+ setInterval(cargarDatos,30000);
+
 }
+
+
