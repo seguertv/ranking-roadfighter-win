724c29fddc0803c0e61737b936d0ef9a4f35fdcf
SEGUER <106338528+seguertv@users.noreply.github.com>
2026-06-14 22:46:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ async function cargarDatos() {
                 ventas: Number(columnas[1]),
                 color: columnas[2]
             });
+
         }
 
         dibujar();
@@ -46,6 +47,93 @@ function dibujar(){
 
     if(asesores.length === 0) return;
 
-    asesores.sort((a,b) => b.ventas - a.ventas);
+    asesores.sort((a,b)=>b.ventas-a.ventas);
 
-    const total
+    const totalAsesores = asesores.length;
+    const alturaUtil = pista.clientHeight - 150;
+
+    asesores.forEach((a,index)=>{
+
+        let icono = "";
+
+        if(index === 0) icono = "🥇";
+        else if(index === 1) icono = "🥈";
+        else if(index === 2) icono = "🥉";
+
+        ranking.innerHTML += `
+            <p>${icono} ${index + 1}. ${a.nombre} (${a.ventas})</p>
+        `;
+
+        const carriles = [20,35,50,65,80];
+        const carril = carriles[index % carriles.length];
+
+        const posicion =
+        20 + ((alturaUtil / totalAsesores) * index);
+
+        const auto = document.createElement("img");
+
+        auto.src = "assets/car.png";
+        auto.className = "auto";
+
+        auto.style.left = carril + "%";
+        auto.style.top = posicion + "px";
+
+        auto.style.filter =
+        `drop-shadow(0 0 8px ${a.color})`;
+
+        pista.appendChild(auto);
+
+        const nombre = document.createElement("div");
+
+        nombre.className = "nombreAuto";
+
+        if(index === 0){
+            nombre.classList.add("top1");
+        }
+        else if(index === 1){
+            nombre.classList.add("top2");
+        }
+        else if(index === 2){
+            nombre.classList.add("top3");
+        }
+
+        if(a.nombre.includes(",")){
+
+            nombre.innerText =
+            a.nombre
+            .split(",")[1]
+            .trim()
+            .split(" ")[0];
+
+        }else{
+
+            nombre.innerText =
+            a.nombre.split(" ")[0];
+
+        }
+
+        nombre.style.left = carril + "%";
+
+        nombre.style.top =
+        (posicion + 78) + "px";
+
+        pista.appendChild(nombre);
+
+    });
+
+    document.getElementById("cuota").innerText = "199";
+
+    document.getElementById("ventasMes").innerText =
+    asesores.reduce(
+        (total,a)=>total+a.ventas,
+        0
+    );
+
+    document.getElementById("hora").innerText =
+    new Date().toLocaleTimeString();
+
+}
+
+cargarDatos();
+
+setInterval(cargarDatos,30000);
