15aa4da72b5ed9ee48c1d7c7a91327f70fbabd17
SEGUER <106338528+seguertv@users.noreply.github.com>
2026-06-14 22:53:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -67,8 +67,25 @@ function dibujar(){
         const carriles = [20,35,50,65,80];
         const carril = carriles[index % carriles.length];
 
-        const posicion =
-        20 + ((alturaUtil / totalAsesores) * index);
+        let posicion;
+
+if(index === 0){
+    posicion = 10;
+}
+else if(index === 1){
+    posicion = 90;
+}
+else if(index === 2){
+    posicion = 170;
+}
+else{
+    posicion =
+    260 + (
+        ((alturaUtil - 260) /
+        (totalAsesores - 3))
+        * (index - 3)
+    );
+}
 
         const auto = document.createElement("img");
 
